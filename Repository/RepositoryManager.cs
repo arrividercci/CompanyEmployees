4c55985caea12c39568fb9236e2958dc1120d6b9
Влад Кучинський <vchelovek0@gmail.com>
2023-08-06 14:08:03 +0300

    added repository manager

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,27 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public sealed class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext context;
+        private readonly Lazy<ICompanyRepository> companyRepository;
+        private readonly Lazy<IEmployeeRepository> employeeRepository;
+        public RepositoryManager(RepositoryContext context)
+        {
+            this.context = context;
+            companyRepository = new Lazy<ICompanyRepository>(() => new CompanyRepository(this.context));
+            employeeRepository = new Lazy<IEmployeeRepository>(() => new EmployeeRepository(this.context));
+        }
+        public ICompanyRepository Company => companyRepository.Value;
+
+        public IEmployeeRepository Employee => employeeRepository.Value;
+
+        public void Save() => context.SaveChanges();
+    }
+}
