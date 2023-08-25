cb07629dadb60bffef2d91059ef4aec4adf0e182
Влад Кучинський <vchelovek0@gmail.com>
2023-08-25 17:35:48 +0300

    implemented crud operations to cotrollers

--- CompanyEmployee.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployee.Presentation/Controllers/CompaniesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CompanyEmployees.Presentation.ModelBinders;
+using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,63 @@ namespace CompanyEmployees.Presentation.Controllers
             var companies = service.CompanyService.GetAllCompanies(trackChanges: false);
 
             return Ok(companies);
+        }
+        [HttpGet("{id:guid}", Name = "CompanyById")]
+        public IActionResult GetCompany(Guid id) 
+        {
+            var company = service.CompanyService.GetCompany(id, trackChanges: false);
+
+            return Ok(company);
+        }
+
+        [HttpPost(Name = "CreateCompany")]
+        public IActionResult CreateCompany([FromBody] CompanyForCreationDto company)
+        {
+            if (company is null)
+            {
+                return BadRequest("CompanyForCreationDto object is null");
+            }
+
+            var createdCompany = service.CompanyService.CreateCompany(company);
+
+            return CreatedAtRoute("CompanyById", new {id = createdCompany.Id}, createdCompany);
+        }
+
+        [HttpGet("collection/({ids})", Name = "CompanyCollection")]
+        public IActionResult GetCompanyCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))]IEnumerable<Guid> ids)
+        {
+            var companies = service.CompanyService.GetByIds(ids, trackChanges: false);
+
+            return Ok(companies);
+        }
+
+        [HttpPost("collection")]
+        public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companies)
+        {
+            var result = service.CompanyService.CreateCompanyCollection(companies);
+
+            return CreatedAtRoute("CompanyCollection", new { result.ids }, result);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteCompany(Guid id)
+        {
+            service.CompanyService.DeleteCompany(id, trackChanges: false);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
+        {
+            if (company is null)
+            {
+                return BadRequest("CompanyForUpdateDto object is null");
+            }
+
+            service.CompanyService.UpdateCompany(id, company, trackChanges: true);
+
+            return NoContent();
         }
     }
 }
---
+++ CompanyEmployee.Presentation/Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Presentation.Controllers
+{
+    [Route("api/companies/{companyId}/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public EmployeeController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmploees(Guid companyId)
+        {
+            var emploees = _service.EmploeeService.GetEmployees(companyId, trackChanges: false);
+
+            return Ok(emploees);
+        }
+
+        [HttpGet("{id:guid}", Name = "GetEmployeeForCompany")]
+        public IActionResult GetEmploees(Guid companyId, Guid id)
+        {
+            var emploee = _service.EmploeeService.GetEmployee(companyId, id, trackChanges: false);
+
+            return Ok(emploee);
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreationDto employee)
+        {
+            if (employee is null)
+            {
+                return BadRequest("EmployeeForCreationDto object is null");
+            }
+            
+            var emploeeToReturn = _service.EmploeeService.CreateEmployeeForCompany(companyId, employee, trackChanges:false);
+
+            return CreatedAtRoute("GetEmployeeForCompany", new { companyId, id = emploeeToReturn.Id }, emploeeToReturn);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id) 
+        {
+            _service.EmploeeService.DeleteEmployeeForCompany(companyId, id, trackChanges: false);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid companyId, Guid id, [FromBody] EmployeeForUpdateDto employee)
+        {
+            if (employee is null)
+            {
+                return BadRequest("EmploeeForUpdateDto object is null");
+            }
+
+            _service.EmploeeService.UpdateEmployeeForCompany(companyId, id, employee, companyTrackChanges: false, employeeTrackChanges: true);
+            
+            return NoContent();
+        }
+    }
+}
