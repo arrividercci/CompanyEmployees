c6c1abe06ec9325f64dce5647657532b901d0635
Влад Кучинський <vchelovek0@gmail.com>
2023-08-25 17:34:52 +0300

    updated company and employee services

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Entities.Models;
 using Service.Contracts;
 using Shared.DataTransferObjects;
@@ -23,6 +24,56 @@ namespace Service
             this._mapper = mapper;
         }
 
+        public CompanyDto CreateCompany(CompanyForCreationDto company)
+        {
+            var companyEntity = _mapper.Map<Company>(company);
+
+            repository.Company.CreateCompany(companyEntity);
+
+            repository.Save();
+
+            var companyToReturn = _mapper.Map<CompanyDto>(companyEntity);
+
+            return companyToReturn;
+        }
+
+        public (IEnumerable<CompanyDto> companies, string ids) CreateCompanyCollection(IEnumerable<CompanyForCreationDto> companyCollection)
+        {
+            if (companyCollection is null) 
+            {
+                throw new CompanyCollectionBadRequest();
+            }
+
+            var companyEntities = _mapper.Map<IEnumerable<Company>>(companyCollection);
+
+            foreach (var companyEntity in companyEntities) 
+            {
+                repository.Company.CreateCompany(companyEntity);
+            }
+
+            repository.Save();
+
+            var companyCollectionToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companyEntities);
+
+            var ids = string.Join(",", companyCollectionToReturn.Select(company => company.Id));
+
+            return (companies: companyCollectionToReturn, ids);
+        }
+
+        public void DeleteCompany(Guid companyId, bool trackChanges)
+        {
+            var company = repository.Company.GetCompany(companyId, trackChanges);
+
+            if (company is null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            repository.Company.DeleteCompany(company);
+
+            repository.Save();
+        }
+
         public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
            
@@ -32,6 +83,53 @@ namespace Service
 
             return companiesDto;
             
+        }
+
+        public IEnumerable<CompanyDto> GetByIds(IEnumerable<Guid> ids, bool trackChanges)
+        {
+            if (ids is null)
+            {
+                throw new CollectionByIdsBadRequestException();
+            }
+
+            var companiesEntities = repository.Company.GetByIds(ids, trackChanges);
+
+            if (ids.Count() != companiesEntities.Count()) 
+            {
+                throw new CollectionByIdsBadRequestException();
+            }
+
+            var companiesToReturn = _mapper.Map<IEnumerable<CompanyDto>>(companiesEntities);
+
+            return companiesToReturn;
+        }
+
+        public CompanyDto GetCompany(Guid companyId, bool trackChanges)
+        {
+            var company = repository.Company.GetCompany(companyId, trackChanges);
+
+            if (company == null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            var companyDto = _mapper.Map<CompanyDto>(company);
+
+            return companyDto;
+        }
+
+        public void UpdateCompany(Guid companyId, CompanyForUpdateDto company, bool trackChanges)
+        {
+            var companyEntity = repository.Company.GetCompany(companyId, trackChanges);
+
+            if (companyEntity is null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            _mapper.Map(company, companyEntity);
+
+            repository.Save();
         }
     }
 }
--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
+using Entities.Models;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +23,105 @@ namespace Service
             this.repository = repository;
             this.logger = logger;
             this._mapper = mapper;
+        }
+
+        public EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employee, bool trackChanges)
+        {
+            var company = repository.Company.GetCompany(companyId, trackChanges);
+
+            if (company is null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            var employeeEntity = _mapper.Map<Employee>(company);
+
+            repository.Employee.CreateEmployeeForCompany(companyId, employeeEntity);
+
+            repository.Save();
+
+            var employeeToReturn = _mapper.Map<EmployeeDto>(employee);
+
+            return employeeToReturn;
+        }
+
+        public void DeleteEmployeeForCompany(Guid companyId, Guid id, bool trackChanges)
+        {
+            var company = repository.Company.GetCompany(companyId, trackChanges);
+
+            if (company is null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            var employeeForCompany = repository.Employee.GetEmployee(companyId, id, trackChanges);
+
+            if (employeeForCompany is null)
+            {
+                throw new EmployeeNotFoundException(id);
+            }
+
+            repository.Employee.DeleteEmployee(employeeForCompany);
+            
+            repository.Save();
+        }
+
+        public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        {
+            var company = repository.Company.GetCompany(companyId, trackChanges);
+
+            if (company == null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            var emploee = repository.Employee.GetEmployee(companyId, id, trackChanges);
+
+            if (emploee == null)
+            {
+                throw new EmployeeNotFoundException(id);
+            }
+
+            var employeeDto = _mapper.Map<EmployeeDto>(emploee);
+
+            return employeeDto;
+        }
+
+        public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+        {
+            var company = repository.Company.GetCompany(companyId, trackChanges);
+
+            if (company == null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            var emploees = repository.Employee.GetEmployees(companyId, trackChanges);
+
+            var emploeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(emploees);
+
+            return emploeesDto;
+        }
+
+        public void UpdateEmployeeForCompany(Guid companyId, Guid id, EmployeeForUpdateDto employee, bool companyTrackChanges, bool employeeTrackChanges)
+        {
+            var company = repository.Company.GetCompany(companyId, companyTrackChanges);
+
+            if(company is null)
+            {
+                throw new CompanyNotFoundException(companyId);
+            }
+
+            var employeeEntity = repository.Employee.GetEmployee(companyId, id, employeeTrackChanges);
+
+            if (employeeEntity is null) 
+            {
+                throw new EmployeeNotFoundException(id);
+            }
+
+            _mapper.Map(employee, employeeEntity);
+
+            repository.Save();
         }
     }
 }
